ef18bf9dae2f9a79a684089e31e968c92be51c1b
RDornodon <archelf.hun@gmail.com>
2021-09-08 12:14:52 +0200

    2nd flex

--- twt_70.py
+++ twt_70.py
@@ -1,15 +1,5 @@
 B='ABCDEFGHIJKLMNOPQRSTUVWXYZ';B+=B.lower()+'0123456789+/=';V=0
 for _ in'_'*int(input()):
  for c in(Q:=input()):V=V*64+B.index(c)
- while V:_=chr(V%256)+_;V//=256
+ while V:_=chr(V%256)+_;V>>=8
  print(_[:~Q.count('=')])
-
-'''7
-aGk=
-Ynll
-bGlnaHQgdw==
-bGlnaHQgd4==
-bGlnaHQgd28=
-bGlnaHQgd2+=
-bGlnaHQgd29y
-'''
